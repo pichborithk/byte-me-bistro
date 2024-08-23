4107ccca5e7c12a8df92f9e27c08b47586ad7afd
Pichborith Kong <pichborith@gmail.com>
2024-08-22 22:34:26 -0700

    Initialize api with Spring Boot

---
+++ api/src/main/java/dev/pichborith/ByteMeBistro/ByteMeBistroApp.java
@@ -0,0 +1,13 @@
+package dev.pichborith.ByteMeBistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteMeBistroApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteMeBistroApp.class, args);
+	}
+
+}
---
+++ api/src/test/java/dev/pichborith/ByteMeBistro/ByteMeBistroAppTests.java
@@ -0,0 +1,13 @@
+package dev.pichborith.ByteMeBistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteMeBistroAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
