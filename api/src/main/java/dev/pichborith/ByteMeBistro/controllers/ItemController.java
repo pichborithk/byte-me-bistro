1706af6df80b0bb53dc3e9d48828853b9a8802ac
Pichborith Kong <pichborith@gmail.com>
2024-08-23 21:42:31 -0700

    add Client url for allow CORS

--- api/src/main/java/dev/pichborith/ByteMeBistro/controllers/ItemController.java
+++ api/src/main/java/dev/pichborith/ByteMeBistro/controllers/ItemController.java
@@ -5,6 +5,7 @@ import dev.pichborith.ByteMeBistro.models.ResponseForm;
 import dev.pichborith.ByteMeBistro.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/items")
+@CrossOrigin(origins = "${CLIENT_URL}")
 public class ItemController {
 
     @Autowired
