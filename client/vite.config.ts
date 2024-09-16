222b612a04e9fed19d82ee5d72f7d0982cd6b5a4
Pichborith Kong <pichborith@gmail.com>
2024-09-16 09:37:11 -0700

    add shadcd ui

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      host: true,
+      strictPort: true,
+      port: Number(env.VITE_PORT),
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  });
+};
