1d243c295ed2e3346d73dc1192a5e74892d5f2c5
Pichborith Kong <pichborith@gmail.com>
2024-09-26 15:07:22 -0700

    rename app file

---
+++ client (iOS SwiftUI)/src/ByteMeBistroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Byte_Me_BistroApp.swift
+//  Byte-Me-Bistro
+//
+//  Created by Pichborith Kong on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ByteMeBistroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppTabView()
+        }
+    }
+}
