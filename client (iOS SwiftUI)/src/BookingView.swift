3040772fa1a7eb7b372d7d1279be6ac52aae416f
Pichborith Kong <pichborith@gmail.com>
2024-09-26 15:08:36 -0700

    create booking view

---
+++ client (iOS SwiftUI)/src/BookingView.swift
@@ -0,0 +1,20 @@
+//
+//  BookingView.swift
+//  Byte-Me-Bistro
+//
+//  Created by Pichborith Kong on 9/26/24.
+//
+
+import SwiftUI
+
+#Preview {
+    BookingView()
+}
+
+struct BookingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
