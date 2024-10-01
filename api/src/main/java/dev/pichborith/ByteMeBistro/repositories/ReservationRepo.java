e8665b72bd92d5fc21d0c72fd3095386f936b18e
Pichborith Kong <pichborith@gmail.com>
2024-10-01 14:54:15 -0700

    refactor Reservation model structure

--- api/src/main/java/dev/pichborith/ByteMeBistro/repositories/ReservationRepo.java
+++ api/src/main/java/dev/pichborith/ByteMeBistro/repositories/ReservationRepo.java
@@ -2,13 +2,10 @@ package dev.pichborith.ByteMeBistro.repositories;
 
 import dev.pichborith.ByteMeBistro.models.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ReservationRepo extends JpaRepository<Reservation, Integer> {
 
     // This is method for query reservations join with users if Fetch Type is Lazy
-    @Query("FROM Reservation r JOIN FETCH r.user")
-    List<Reservation> findAllWithUser();
+//    @Query("FROM Reservation r JOIN FETCH r.user")
+//    List<Reservation> findAllWithUser();
 }
