1672029af5c11c41eada590d73173d4300d797a1
Pichborith Kong <pichborith@gmail.com>
2024-10-01 13:22:53 -0700

    create item quantity button

---
+++ client (iOS SwiftUI)/src/ItemQuantityButton.swift
@@ -0,0 +1,62 @@
+//
+//  ItemQuantityButton.swift
+//  Byte-Me-Bistro
+//
+//  Created by Pichborith Kong on 10/1/24.
+//
+
+import SwiftUI
+
+#Preview {
+    ItemQuantityButton(quantity: .constant(1))
+}
+
+struct ItemQuantityButton: View {
+    @Binding var quantity: Int
+
+    var body: some View {
+        HStack(spacing: 24) {
+            Button {
+                decrement()
+            } label: {
+                Text("-")
+                    .font(.title)
+                    .padding()
+            }
+            .disabled(quantity <= 1)
+
+            Text("\(quantity)")
+                .bold()
+                .font(.title3)
+                .foregroundStyle(Color.accentColor)
+
+            Button {
+                increment()
+            } label: {
+                Text("+")
+                    .font(.title)
+                    .padding()
+            }
+            .disabled(quantity >= 9)
+        }
+        .frame(maxWidth: .infinity, maxHeight: 54)
+        .cornerRadius(10) // Round the corners of the background
+        .overlay( // Add a border using an overlay with a RoundedRectangle
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.accentColor, lineWidth: 2))
+        .controlSize(.large)
+      
+    }
+    
+    func increment() {
+        if quantity < 9 {
+            quantity += 1
+        }
+    }
+    
+    func decrement() {
+        if quantity > 1 {
+            quantity -= 1
+        }
+    }
+}
