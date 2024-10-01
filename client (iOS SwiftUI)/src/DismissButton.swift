5e4cb47a038d83fa011a0175342fda428f57f670
Pichborith Kong <pichborith@gmail.com>
2024-10-01 13:22:38 -0700

    create dismiss button

---
+++ client (iOS SwiftUI)/src/DismissButton.swift
@@ -0,0 +1,32 @@
+//
+//  DismissButton.swift
+//  Byte-Me-Bistro
+//
+//  Created by Pichborith Kong on 10/1/24.
+//
+
+import SwiftUI
+
+#Preview {
+    DismissButton(isPresented: .constant(true))
+}
+
+struct DismissButton: View {
+    @Binding var isPresented: Bool
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            Button {
+                isPresented = false
+                print("tapped")
+            } label: {
+                Image(systemName: "xmark")
+                    .foregroundColor(Color(.label))
+                    .imageScale(.large)
+                    .frame(width: 40, height: 40)
+            }
+        }
+    }
+}
