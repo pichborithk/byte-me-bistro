bf77297afb0c8bb1b282e654a9dc771e1503b8d8
Pichborith Kong <pichborith@gmail.com>
2024-10-02 23:18:31 -0700

    string extension example for formatting phone number

---
+++ client (iOS SwiftUI)/src/StringExtension.swift
@@ -0,0 +1,31 @@
+//
+//  StringExtension.swift
+//  Byte-Me-Bistro
+//
+//  Created by Pichborith Kong on 10/1/24.
+//
+
+import Foundation
+
+extension String {
+    func formatPhoneNumber() -> String {
+        let cleanNumber = components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        
+        let mask = "(XXX) XXX-XXXX"
+        
+        var result = ""
+        var startIndex = cleanNumber.startIndex
+        let endIndex = cleanNumber.endIndex
+        
+        for char in mask where startIndex < endIndex {
+            if char == "X" {
+                result.append(cleanNumber[startIndex])
+                startIndex = cleanNumber.index(after: startIndex)
+            } else {
+                result.append(char)
+            }
+        }
+        
+        return result
+    }
+}
