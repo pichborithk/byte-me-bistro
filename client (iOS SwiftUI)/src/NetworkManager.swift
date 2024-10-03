e818a2d06a94b8b5da09d70136a711ce04d3fded
Pichborith Kong <pichborith@gmail.com>
2024-10-02 23:19:25 -0700

    create reservation function

--- client (iOS SwiftUI)/src/NetworkManager.swift
+++ client (iOS SwiftUI)/src/NetworkManager.swift
@@ -8,22 +8,47 @@
 import UIKit
 
 class NetworkManager {
-    
     static let shared = NetworkManager()
     private let baseUrl = "http://localhost:1337/api"
-    
-    func getMenu() async throws-> [Item] {
+
+    func getMenu() async throws -> [Item] {
         guard let url = URL(string: baseUrl + "/items") else {
             throw RequestError.invalidURL
         }
-        
+
         let (data, _) = try await URLSession.shared.data(from: url)
-        print(data)
+ 
         do {
             let result = try JSONDecoder().decode(APIResponse<[Item]>.self, from: data)
             return result.data
         } catch {
             throw RequestError.invalidData
         }
+    }
+
+    func createReservation(from reservation: ReservationRequest) async throws {
+        guard let url = URL(string: baseUrl + "/reservations") else {
+            throw RequestError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        guard let JSONData = try? JSONEncoder().encode(reservation) else {
+            throw RequestError.invalidData
+        }
+        
+        request.httpBody = JSONData
+        
+        guard let (data, _) = try? await URLSession.shared.data(for: request) else {
+            throw RequestError.requestFailed
+        }
+        
+        guard let result = try? JSONDecoder().decode(ReservationResponse.self, from: data) else {
+            throw RequestError.invalidData
+        }
+    
+        print(result)
     }
 }
